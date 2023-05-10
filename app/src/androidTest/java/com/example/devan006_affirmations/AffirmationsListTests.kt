853da6f8ea40d9c4527e0a5f352417f727b38a3e
Tabuletto <robomez@mail.ru>
2023-05-11 00:31:49 +0300

    Created unit test AffirmationsListTests.kt file

---
+++ app/src/androidTest/java/com/example/devan006_affirmations/AffirmationsListTests.kt
@@ -0,0 +1,10 @@
+package com.example.devan006_affirmations
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.runner.RunWith
+
+class AffirmationsListTests {
+    @RunWith(AndroidJUnit4::class)
+
+
+}
